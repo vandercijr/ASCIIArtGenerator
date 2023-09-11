b5c8e34b836a68ac5391b40529b96b3d1f252ca0
Vanderci Curvelo Jr <vanderci.curvelo@oplab.com>
2023-09-11 20:58:01 -0300

    Add frequent render && remove useless

--- art.js
+++ art.js
@@ -4,31 +4,21 @@ const rangeMap = (value, a, b, c, d) => {
   return Math.floor(value * scale + offset);
 };
 
-const renderAscii = (input, output, canvas, invert = false) => {
+const renderAscii = (input, output, invert = false) => {
   const PIXEL_SIZE = 4;
   const ascii = "Ñ@#W$9876543210?!abc;:+=-,._  ";
-  // const ascii =
-  //   String.fromCharCode(9618) +
-  //   "ME@#W$987650?!abcsj;:+=-,.   " +
-  //   String.fromCharCode(8801);
-
-  // const unicodes = [
-  //   9619, 9618, 9617, 8801, 182, 198, 171, 8249, 8218, 9472, 8211, 8212,
-  //   176, 8226, 164, 169,
-  // ];
-  // const ascii = unicodes.map((u) => String.fromCharCode(u)).join("");
 
   const _ascii = invert ? ascii.split("").reverse().join("") : ascii;
 
-  const context = canvas.getContext("2d");
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d", {
+    willReadFrequently: true,
+  });
 
   context.drawImage(input, 0, 0);
 
   const imageData = context.getImageData(0, 0, input.width, input.height);
   const { data: pixels } = imageData;
-
-  const w = canvas.width / input.width;
-  const h = canvas.height / input.height;
 
   output.innerHTML = "";
   for (let j = 0, s = input.height; j < s; j++) {
@@ -42,13 +32,8 @@ const renderAscii = (input, output, canvas, invert = false) => {
       ];
       const grey = (r + g + b) / 3;
 
-      context.fillStyle = `rgb(${grey}, ${grey}, ${grey})`;
-      context.fillRect(i * w, j * h, w, w);
-      context.textBaseline = "middle";
-      context.textAlign = "center";
-
       const c = _ascii.charAt(rangeMap(grey, 0, 255, _ascii.length, 0));
-      context.fillText(c, i * w + w * 0.5, j * h + h * 0.5);
+
       dataOutput += c === " " ? "&nbsp" : c;
     }
     dataOutput += "<br/>";
